006b2b415a60de0a5d89bdabfd977ffa4b34a65e
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-02-26 20:52:57 -0300

    main

---
+++ Bubble/Bubble/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Bubble.Models;
+
+public class User
+{
+    public User(){}
+    public string name { get; set; }
+    public string image { get; set; }
+    public Color color { get; set; }
+
+    public User(string name, string image, Color color)
+    {
+        this.name = name;
+        this.image = image;
+        this.color = color;
+    }
+}
