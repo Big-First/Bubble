39e48d35e7ad845651ab70f8080cd405800dadee
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-02-27 12:14:01 -0300

    update

---
+++ Bubble/Bubble/Views/DetailView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bubble.Views;
+
+public partial class DetailView : ContentPage
+{
+    public DetailView()
+    {
+        InitializeComponent();
+        NavigationPage.SetHasNavigationBar(this, false);
+    }
+}
