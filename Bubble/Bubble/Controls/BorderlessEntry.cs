643795e475d34a207d2025e7f4dade297bb590dc
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-03-19 15:04:52 -0300

    up

--- Bubble/Bubble/Controls/BorderlessEntry.cs
+++ Bubble/Bubble/Controls/BorderlessEntry.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bubble.Controls
+﻿namespace Bubble.Controls
 {
     public class BorderlessEntry : Entry
     {
